cbee92977b18caaf9a1f4bc1ec0a7cb44b0d7e24
junayedhasan302 <junayedhasan302@gmail.com>
2026-07-28 05:06:39 +0600

    8

---
+++ conc3jsPrbAdvnc-8.js
@@ -0,0 +1,7 @@
+// 8. Write a function that takes two numbers as parameters and returns the larger of the two without using Math.max().
+function largerNumber(a, b) {
+    if (a > b) {
+        return a;
+    } return b;
+}
+console.log(largerNumber(15, 30));
