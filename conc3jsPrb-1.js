d9134fb8e54cf87f83d26c425e15d3794b525d34
junayedhasan302 <junayedhasan302@gmail.com>
2026-07-28 04:44:39 +0600

    Problem 1,2

---
+++ conc3jsPrb-1.js
@@ -0,0 +1,6 @@
+// 1. Given a string, print it in both uppercase and lowercase using the appropriate string methods.
+
+let str = "JavaScript";
+
+console.log("Uppercase:", str.toUpperCase());
+console.log("Lowercase:", str.toLowerCase());
---
+++ conc3jsPrb-2.js
@@ -0,0 +1,5 @@
+// 2. Given a string with leading and trailing spaces, print its length before trimming and after trimming to show the difference.
+let str = "   Hello JavaScript   ";
+console.log("Before Trim:", str.length);
+let trimmed = str.trim();
+console.log("After Trim:", trimmed.length);
