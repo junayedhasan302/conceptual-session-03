6db98915b39b45beaf6fb482bfd73338332db7e9
junayedhasan302 <junayedhasan302@gmail.com>
2026-07-28 04:48:03 +0600

    Problem 5

---
+++ conc3jsPrb-5.js
@@ -0,0 +1,11 @@
+// 5. Create an object representing a product with name, price, and quantity, then update the price using bracket notation and
+// print the updated object.
+let product = {
+    name: "Laptop",
+    price: 850,
+    quantity: 5
+};
+
+product["price"] = 900;
+
+console.log(product);
