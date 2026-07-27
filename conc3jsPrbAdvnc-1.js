32b3a271de5122eae17f7db5219c019a4b392774
junayedhasan302 <junayedhasan302@gmail.com>
2026-07-28 04:58:30 +0600

    1,2

---
+++ conc3jsPrbAdvnc-1.js
@@ -0,0 +1,6 @@
+// 1. Write a function that takes a person's name and age as parameters and returns a formatted introduction sentence.
+function introduce(name, age) {
+    return "My name is " + name + " and I am " + age + " years old.";
+}
+
+console.log(introduce("Junayed", 24));
---
+++ conc3jsPrbAdvnc-2.js
@@ -0,0 +1,6 @@
+// 2. Write a function that takes the length and width of a rectangle as parameters and returns its area.
+function rectangleArea(length, width) {
+    return length * width;
+}
+
+console.log(rectangleArea(10, 5));
