de8defa42fab1456160a642400b343b293ca93d7
junayedhasan302 <junayedhasan302@gmail.com>
2026-07-29 00:36:49 +0600

    Extras 2

---
+++ extras2.js
@@ -0,0 +1,14 @@
+// 2. Write a function that takes a string and returns true if it is a palindrome; otherwise, return false.
+// Example: "madam" → true, "hello" → false
+function isPalindrome(str) {
+    let reverse = "";
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        reverse += str[i];
+    }
+
+    return str === reverse;
+}
+
+console.log(isPalindrome("madam"));
+console.log(isPalindrome("hello"));
